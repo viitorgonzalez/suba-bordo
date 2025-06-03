1c6356001b90395b2b2cd7e5b3ca44e2f1fc6cd6
viitorgonzalez <pastor123.vs@gmail.com>
2025-06-03 16:41:35 -0300

    initial commit

---
+++ backend/src/test/java/com/subabordo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subabordo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
